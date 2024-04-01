b63216a3053fe1133dc09743a5fc9087ec35597d
Jack T <jack@jackt.space>
2024-04-01 14:30:03 -0700

    Cleanup

--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,3 @@
-use bson::DateTime;
 use chrono::Utc;
 use serde::{Deserialize, Serialize};
 
@@ -10,8 +9,8 @@ pub struct BanDocument {
 
     pub reason: String,
 
-    pub expires: Option<DateTime>,
-    pub timestamp: DateTime,
+    pub expires: Option<bson::DateTime>,
+    pub timestamp: bson::DateTime,
 }
 
 impl BanDocument {
@@ -20,8 +19,8 @@ impl BanDocument {
             user_id,
             moderator_id: request.moderator_id,
             reason: request.reason,
-            expires: request.expires.map(|date| DateTime::from_chrono(date)),
-            timestamp: DateTime::now(),
+            expires: request.expires.map(bson::DateTime::from_chrono),
+            timestamp: bson::DateTime::now(),
         }
     }
 }
@@ -67,35 +66,6 @@ impl From<BanDocument> for V1Ban {
                 .expires
                 .map(|date| date.try_to_rfc3339_string().unwrap()),
             timestamp: document.timestamp.try_to_rfc3339_string().unwrap(),
-        }
-    }
-}
-
-mod opt_bson_datetime_as_rfc3339_string {
-    use bson::DateTime;
-    use serde::{Deserialize, Deserializer, Serialize, Serializer};
-
-    pub fn serialize<S>(date: &Option<DateTime>, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        match date {
-            Some(date) => date.try_to_rfc3339_string().unwrap().serialize(serializer),
-            None => serializer.serialize_none(),
-        }
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<DateTime>, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let date = String::deserialize(deserializer)?;
-        if date.is_empty() {
-            Ok(None)
-        } else {
-            Ok(Some(
-                DateTime::parse_rfc3339_str(&date).map_err(serde::de::Error::custom)?,
-            ))
         }
     }
 }
