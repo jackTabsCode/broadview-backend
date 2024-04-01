e622bb4f7acfdccd0f7b534a43665983484c6c47
Jack T <jack@jackt.space>
2024-04-01 03:05:51 -0700

    What is this deserializer??????????

--- src/models.rs
+++ src/models.rs
@@ -10,6 +10,7 @@ pub struct Ban {
     pub reason: String,
 
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(with = "opt_bson_datetime_as_rfc3339_string")]
     pub expires: Option<DateTime>,
 
     #[serde(with = "bson::serde_helpers::bson_datetime_as_rfc3339_string")]
@@ -28,4 +29,33 @@ pub enum V1BanResult {
         #[serde(flatten)]
         ban: Ban,
     },
+}
+
+mod opt_bson_datetime_as_rfc3339_string {
+    use bson::DateTime;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    pub fn serialize<S>(date: &Option<DateTime>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match date {
+            Some(date) => date.try_to_rfc3339_string().unwrap().serialize(serializer),
+            None => serializer.serialize_none(),
+        }
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<DateTime>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let date = String::deserialize(deserializer)?;
+        if date.is_empty() {
+            Ok(None)
+        } else {
+            Ok(Some(
+                DateTime::parse_rfc3339_str(&date).map_err(serde::de::Error::custom)?,
+            ))
+        }
+    }
 }
