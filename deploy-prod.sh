cea9295aca092c0f883bdcbb992b40d8bac16aa6
Jack T <jack@jackt.space>
2024-04-01 20:17:41 -0700

    Deploy script

---
+++ deploy-prod.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -x
+
+BIN=broadview-backend
+REMOTE=ubuntu@147.135.37.165
+RELEASE_DIR="~/broadview-backend/releases/$GITHUB_SHA"
+
+ssh $REMOTE "mkdir -p $RELEASE_DIR"
+scp release.zip $REMOTE:$RELEASE_DIR/release.zip
+
+ssh $REMOTE /bin/bash << EOF
+set -x
+cd $RELEASE_DIR
+echo "Hello world" > hello.txt
+# unzip -u release.zip
+# rm release.zip
+
+# cd ~/broadview-backend
+# if test -f "$BIN"; then
+# 	mv $BIN $BIN-old
+# fi
+# cp $RELEASE_DIR/$BIN ./$BIN
+
+# systemctl restart broadview-backend
+EOF
